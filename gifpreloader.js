989ccad65570fcacd229c6b78a959c13132aef9d
Jack Wakefield <xadetthree@gmail.com>
2013-02-16 22:44:36 +0000

    Fixed being unable to zoom when viewing images

--- gifpreloader.js
+++ gifpreloader.js
@@ -38,7 +38,8 @@
 
             this.$image = $(this.element);
             this.source = this.$image.prop('src');
-            this.hasImageLoaded = false;
+            this.$placeholder = null;
+            this.reloadedImage = false;
 
             this.$image.load(function() {
                 self.imageLoaded();
@@ -46,8 +47,7 @@
 
             // ensure the image isn't cached and that the image is an absolute or relative path
             // and not inline data
-            if (!this.element.complete && this.element.readyState !== 'complete' &&
-                this.element.readyState !== 4 && (this.source.substr(0, 4) === 'http' ||
+            if (!this.hasImageLoaded() && (this.source.substr(0, 4) === 'http' ||
                 this.source.substr(0, 1) === '/')) {
                 this.retrieveImageHeader();
             }
@@ -83,16 +83,18 @@
          * Called when the image has been loaded.
          */
         imageLoaded: function() {
-            if (!this.hasImageLoaded) {
-                // if the image has not been loaded before, reset the src attributes to re-play the
-                // animation
-                this.hasImageLoaded = true;
-                this.$image.attr('src', this.source);
-            } else {
-                // if the image has been loaded before then remove the placeholder and unwrap the
-                // image element as the animation has been restarted
-                this.$placeholder.remove();
-                this.$image.unwrap();
+            if (this.$placeholder !== null) {
+                if (!this.reloadedImage) {
+                    // if the image has not been loaded before, reset the src attributes to re-play
+                    // the animation
+                    this.reloadedImage = true;
+                    this.$image.attr('src', this.source);
+                } else {
+                    // if the image has been loaded before then remove the placeholder and unwrap
+                    // the image element as the animation has been restarted
+                    this.$placeholder.remove();
+                    this.$image.unwrap();
+                }
             }
         },
 
@@ -101,12 +103,16 @@
          */
         createPlaceholder: function() {
             // ensure the image hasn't been loaded
-            if (!this.hasImageLoaded) {
+            if (!this.hasImageLoaded()) {
                 // set the width and height attributes to the image size if they are not present
                 // to allow the overlay to have fixed dimensions
                 if (!this.$image.attr('width') && !this.$image.attr('height')) {
                     this.$image.attr('width', this.getImageWidth());
                     this.$image.attr('height', this.getImageHeight());
+                    this.$image.css('min-width', '');
+                    this.$image.css('max-width', '');
+                    this.$image.css('min-height', '');
+                    this.$image.css('max-height', '');
                 }
 
                 // create the overlay container element to wrap around the image
@@ -165,6 +171,15 @@
             }
 
             return this.dataView.getUint16(8, true);
+        },
+
+        /**
+         * Determines whether the image has loaded.
+         * @return {Boolean} A value determining whether the image has loaded.
+         */
+        hasImageLoaded: function() {
+            return this.element.complete || this.element.readyState === 'complete' ||
+                this.element.readyState === 4;
         }
     };
 
